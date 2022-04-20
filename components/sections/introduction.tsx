e491ebf1178b6b9805fb2033c34133dddf9edae4
Tim Denning <36028402+denningta@users.noreply.github.com>
2022-04-20 11:58:21 +0200

    changed styling in introrudction section

--- components/sections/introduction.tsx
+++ components/sections/introduction.tsx
@@ -9,23 +9,27 @@ interface Props {
 
 function Introduction({ data }: Props) {
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col items-center max-w-xl my-10">
-        <div>
-          {data.image && (
-            <Image
-              className="rounded-full overflow-clip shadow-lg"
-              width={300}
-              height={300}
-              alt={`Cover Image for ${data.headline}`}
-              src={imageBuilder(data.image).width(800).height(800).url()}
-            />
-          )}
-        </div>
-        <div className="mt-6 text-3xl font-extrabold">{data.tagline}</div>
-        <div className="mt-6 text-xl">{data.headline}</div>
-        <div className="mt-5 leading-8 portable-text">
-          {data.introduction && <PortableText value={data.introduction} />}
+    <div className="flex justify-center px-4 border-b border-neutral-800 bg-neutral-800">
+      <div className="flex flex-col items-center max-w-primary-col my-10">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="flex flex-col items-center">
+            <div className="mt-6 text-3xl font-extrabold">{data.tagline}</div>
+            <div className="mt-6 text-xl">{data.headline}</div>
+            <div className="mt-6">
+              {data.image && (
+                <Image
+                  className="rounded-full overflow-clip shadow-lg"
+                  width={260}
+                  height={260}
+                  alt={`Cover Image for ${data.headline}`}
+                  src={imageBuilder(data.image).width(800).height(800).url()}
+                />
+              )}
+            </div>
+          </div>
+          <div className="mt-5 leading-8 portable-text">
+            {data.introduction && <PortableText value={data.introduction} />}
+          </div>
         </div>
       </div>
     </div>
